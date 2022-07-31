478bf10ea994d697b8095891c55d1b7c80d5fd19
t-cool <hiroki802@hotmail.com>
2022-07-31 15:40:59 +0900

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# phonetic-symbols-to-English-text
+
+This program converts English text into the International Phonetic Alphabet (IPA) using the Pronouncing Dictionary published by Carnegie-Mellon University.
+
+This project was based on the [mphilli/English-to-IPA](https://github.com/mphilli/English-to-IPA) project.
+
+
+## Usage
+
+![screencasts.gif](./screencasts.gif)
+
+It is available on the GitHub Page at https://t-cool.github.io/English-to-IPA/ .
+
+Please paste the sentences you want to add phonetic symbols to and press the convert button. You can also click on each word to have it read aloud.
+
+## License
+
+MIT
---
+++ main.js
@@ -0,0 +1,92 @@
+// Fetching resources
+const requestURL = "https://t-cool.github.io/English-to-IPA/dict.json";
+const request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'text';
+request.send();
+
+// initial settings for TTS
+const voice = new talkify.Html5Player();
+voice.forceLanguage('en');
+voice.setRate(1);
+
+request.onload = function() {
+  // DB
+  let asset = JSON.parse(request.response);
+  asset = asset[0];
+
+  // auxiliary function
+  function remove_punc(words){
+    let words_preserved = [];
+    words = words.split(" ");
+    for (w of words) {
+      lowerWord = w.toLowerCase();
+      let punct_str = lowerWord.replace(/\!|\(|\)|\"|\#|\$|\%|\&|\\|\'|\(|\)|\*|\+|\,|\-|\.|\/|\:|\;|\<|\=|\>|\/|\?|\@|\[|\\|\\|\]|\^|\_|\`|\{|\||\}|\~|\«|\»|\ /g, '');
+      words_preserved.push([w, punct_str]);
+    }
+    return words_preserved
+  }
+
+  function get_cmu(words_array){
+  let result = words_array.map(wordArray => {
+      wordArray[1] = asset[wordArray[1]];
+      if(wordArray[1]==undefined){
+        wordArray[1] = [`*${wordArray[0]}*`];
+      }
+
+      return wordArray
+    })
+
+  return result;
+  }
+
+  function convert(words_in){
+    // converts a string to an array format
+    let words_array = remove_punc(words_in);
+    // =>　[['Are', 'are'],['you', 'you'] ]
+
+    let cmu_words_array = get_cmu(words_array);
+    // =>　[['Are, ['ɑr', 'ər']], ['You',['ju']] ]
+
+    return cmu_words_array
+  }
+
+  // UI
+  document.getElementById("convert").addEventListener('click',(e)=>{
+    let inputText = document.querySelector("#text").value;
+    let converted_data = convert(inputText);
+    for (wordIPA of converted_data){
+      makeBox(wordIPA[0],wordIPA[1]);
+    }
+
+    hideElements(); 
+  })
+
+  function makeBox(word, sounds){
+    const boxDom = document.createElement("form");
+    boxDom.className = "item";
+    containerB.appendChild(boxDom);
+
+    const wordDom = document.createElement("div");
+
+    // word UI
+    wordDom.innerText = word;
+    boxDom.appendChild(wordDom);
+
+    // options UI
+    const soundsDom = document.createElement("select");
+    sounds.map(value => soundsDom.append(new Option(value)));
+
+    boxDom.appendChild(soundsDom);
+    boxDom.addEventListener('click',function(e){
+      // TTS
+      voice.playText(word);
+    })
+  }
+
+  function hideElements(){
+    document.getElementById("caption").style.display = "none";
+    document.getElementById("convert").style.display = "none";
+    document.getElementById("text").style.display = "none";
+  }
+}
